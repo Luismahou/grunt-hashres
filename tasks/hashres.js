005826789aee9ba2c9e6e4f30c9b52a546817d41
Luismahou <luisma@smartsparrow.com>
2012-10-26 16:10:31 +1100

    Removing sample grunt tasks.

--- tasks/hashres.js
+++ tasks/hashres.js
@@ -8,23 +8,66 @@
 
 module.exports = function(grunt) {
 
-  // Please see the grunt documentation for more information regarding task and
-  // helper creation: https://github.com/gruntjs/grunt/blob/master/docs/toc.md
+  // Loading necessary modules
+  var fs = require('fs'),
+      path = require('path'),
+      crypto = require('crypto');
 
-  // ==========================================================================
-  // TASKS
-  // ==========================================================================
-
-  grunt.registerTask('hashres', 'Your task description goes here.', function() {
-    grunt.log.write(grunt.helper('hashres'));
+  grunt.registerMultiTask(
+      'hashres',
+      'Hashes your resources and updates the files that refer to them',
+      function() {
+    // Required properties: 'files' and 'dest'
+    this.requiresConfig(this.name + '.' + this.target + '.files');
+    this.requiresConfig(this.name + '.' + this.target + '.dest');
+    grunt.helper('hashAndSub', this.data.files, this.data.dest, this.data.encoding);
   });
+  grunt.registerHelper('hashAndSub', function(files, dest, encoding) {
+    var nameToHashedName = {};
+    encoding = (encoding || 'utf8');
+    grunt.log.debug('Using encoding ' + encoding);
 
-  // ==========================================================================
-  // HELPERS
-  // ==========================================================================
+    // Converting the file to an array if is only one
+    files = Array.isArray(files) ? files : [files];
+    // Renaming the files using a unique name
+    grunt.file.expand(files).forEach(function(f) {
+      var md5 = grunt.helper('md5', f),
+          fileName = path.basename(f),
+          lastIndex = fileName.lastIndexOf('.'),
+          renamed = [
+            md5.slice(0, 8), // Starging the file with the hash that would make it unique
+            fileName.slice(0, lastIndex), // Real name of the file
+            'cache', // Good to have to ease apache configuration to enable 'perfect caching'
+            fileName.slice(lastIndex + 1, fileName.length) // Extension of the file
+          ].join('.');
 
-  grunt.registerHelper('hashres', function() {
-    return 'hashres!!!';
+      // Mapping the original name with hashed one for later use.
+      nameToHashedName[fileName] = renamed;
+
+      // Renaming the file
+      fs.renameSync(f, path.resolve(path.dirname(f), renamed));
+      grunt.log.write(f + ' ').ok(renamed);
+    });
+
+    // Substituting references to the given files with the hashed ones.
+    var destContents = fs.readFileSync(dest, encoding);
+    for (var name in nameToHashedName) {
+      grunt.log.debug('Substituting ' + name + ' by ' + nameToHashedName[name]);
+      destContents = destContents.replace(name, nameToHashedName[name]);
+    }
+    grunt.log.debug('Saving the updated contents of the destination file');
+    fs.writeFileSync(dest, destContents, encoding);
+  });
+  // **md5** helper is a basic wrapper around crypto.createHash, with
+  // given `algorithm` and `encoding`. Both are optional and defaults to
+  // `md5` and `hex` values.
+  grunt.registerHelper('md5', function(filepath, algorithm, encoding) {
+    algorithm = algorithm || 'md5';
+    encoding = encoding || 'hex';
+    var hash = crypto.createHash(algorithm);
+    hash.update(grunt.file.read(filepath));
+    grunt.log.verbose.write('Hashing ' + filepath + '...');
+    return hash.digest(encoding);
   });
 
 };
