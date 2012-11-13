36f69d90b8b7b62098fed600f0a5555919834eb2
luismahou <luismahou@gmail.com>
2012-11-14 00:52:44 +1100

    Added test with subfolders.

--- test/hashresHelper.spec.js
+++ test/hashresHelper.spec.js
@@ -29,6 +29,26 @@ vows.describe('hashres').addBatch({
       assert(path.existsSync('./temp/simple/5a7a5b61.myscripts.cache.js'));
       var html = fs.readFileSync('./temp/simple/index.html', 'utf8');
       assert(html.indexOf('5a7a5b61.myscripts.cache.js') !== -1);
+    },
+    'for sample with subfolders': function(grunt) {
+      helper.hashAndSub(
+        grunt, {
+          files: [
+            './temp/subfolders/scripts/*.js',
+            './temp/subfolders/styles/mystyles1.css',
+            './temp/subfolders/styles/mystyles2.css'],
+          out: './temp/subfolders/index.html',
+          fileNameFormat: '${hash}-${name}.${ext}'
+        });
+      assert(path.existsSync('./temp/subfolders/scripts/5a7a5b61-myscripts1.js'));
+      assert(path.existsSync('./temp/subfolders/scripts/5a7a5b61-myscripts2.js'));
+      assert(path.existsSync('./temp/subfolders/styles/3b97b071-mystyles1.css'));
+      assert(path.existsSync('./temp/subfolders/styles/3b97b071-mystyles2.css'));
+      var html = fs.readFileSync('./temp/subfolders/index.html', 'utf8');
+      assert(html.indexOf('scripts/5a7a5b61-myscripts1.js') !== -1);
+      assert(html.indexOf('scripts/5a7a5b61-myscripts2.js') !== -1);
+      assert(html.indexOf('styles/3b97b071-mystyles1.css') !== -1);
+      assert(html.indexOf('styles/3b97b071-mystyles2.css') !== -1);
     }
   }
 }).export(module);
