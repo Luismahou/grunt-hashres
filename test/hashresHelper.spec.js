7c9e98b5ca6d265ccb0173073580ddb5abac02d4
domenicoven <Domenico.Ventura@deltatre.com>
2016-02-03 15:16:14 +0100

    Using hashres also in query string: added tests to prove the regex was not matching the query string, solved the bug, all tests still passing

--- test/hashresHelper.spec.js
+++ test/hashresHelper.spec.js
@@ -60,6 +60,64 @@ vows.describe('hashresHelper').addBatch({
       assert(html.indexOf('scripts/5a7a5b61-myscripts2.js') !== -1);
       assert(html.indexOf('styles/3b97b071-mystyles1.css') !== -1);
       assert(html.indexOf('styles/3b97b071-mystyles2.css') !== -1);
+    },
+'for using has as querystring': function(grunt) {
+      helper.hashAndSub(
+        grunt, {
+          files: [{
+            src : grunt.file.expand([
+              './temp/helper/subfolders-querystring/scripts/*.js',
+              './temp/helper/subfolders-querystring/styles/mystyles1.css',
+              './temp/helper/subfolders-querystring/styles/mystyles2.css']),
+            dest: './temp/helper/subfolders-querystring/index.html'
+          }],
+          fileNameFormat: '${name}.${ext}?v=${hash}',
+          encoding      : 'utf8',
+          renameFiles   : true
+        });
+      var html = fs.readFileSync('./temp/helper/subfolders-querystring/index.html', 'utf8');
+      assert(html.indexOf('scripts/myscripts1.js?v=5a7a5b61') !== -1);
+      assert(html.indexOf('scripts/myscripts2.js?v=5a7a5b61') !== -1);
+      assert(html.indexOf('styles/mystyles1.css?v=3b97b071') !== -1);
+      assert(html.indexOf('styles/mystyles2.css?v=3b97b071') !== -1);
+    },
+'for using has as querystring twice or multiple times': function(grunt) {
+      helper.hashAndSub(
+        grunt, {
+          files: [{
+            src : grunt.file.expand([
+              './temp/helper/subfolders-querystring2/scripts/*.js',
+              './temp/helper/subfolders-querystring2/styles/mystyles1.css',
+              './temp/helper/subfolders-querystring2/styles/mystyles2.css']),
+            dest: './temp/helper/subfolders-querystring2/index.html'
+          }],
+          fileNameFormat: '${name}.${ext}?v=${hash}',
+          encoding      : 'utf8',
+          renameFiles   : true
+        });
+
+/*
+      helper.hashAndSub(
+        grunt, {
+          files: [{
+            src : grunt.file.expand([
+              './temp/helper/subfolders-querystring2/scripts/*.js',
+              './temp/helper/subfolders-querystring2/styles/mystyles1.css',
+              './temp/helper/subfolders-querystring2/styles/mystyles2.css']),
+            dest: './temp/helper/subfolders-querystring2/index.html'
+          }],
+          fileNameFormat: '${name}.${ext}?v=${hash}',
+          encoding      : 'utf8',
+          renameFiles   : true
+        });
+
+*/
+      var html = fs.readFileSync('./temp/helper/subfolders-querystring2/index.html', 'utf8');
+      assert(html.indexOf('"scripts/myscripts1.js?v=5a7a5b61"') !== -1);
+      assert(html.indexOf('"scripts/myscripts2.js?v=5a7a5b61"') !== -1);
+      assert(html.indexOf('"styles/mystyles1.css?v=3b97b071"') !== -1);
+      assert(html.indexOf('"styles/mystyles2.css?v=3b97b071"') !== -1);
     }
-  }
+}
+
 }).export(module);
